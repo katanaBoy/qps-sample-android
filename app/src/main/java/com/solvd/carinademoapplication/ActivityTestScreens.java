b2a024676b398c233ef8982b425d33fe9c05609e
Artyom Seliuzhitsky <artyom.seliuzhistky@smule.com>
2019-03-27 17:55:06 +0300

    + Added Ad

--- app/src/main/java/com/solvd/carinademoapplication/ActivityTestScreens.java
+++ app/src/main/java/com/solvd/carinademoapplication/ActivityTestScreens.java
@@ -1,5 +1,7 @@
 package com.solvd.carinademoapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -9,15 +11,21 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.solvd.carinademoapplication.test_screens.FragmentCharts;
 import com.solvd.carinademoapplication.test_screens.FragmentMap;
 import com.solvd.carinademoapplication.test_screens.FragmentUiElements;
 import com.solvd.carinademoapplication.test_screens.FragmentWebView;
 
+import java.util.ArrayList;
+
 public class ActivityTestScreens extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
+    private ImageSlider imageSlider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,9 @@ public class ActivityTestScreens extends AppCompatActivity {
         setContentView(R.layout.activity_test_screens);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+
+        imageSlider = findViewById(R.id.image_slider);
+        initImageSlider();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -43,6 +54,48 @@ public class ActivityTestScreens extends AppCompatActivity {
         });
 
         setInitialScreenState(navigationView);
+    }
+
+    private void handleAdClicked(int position) {
+        String siteLink;
+
+        switch (position) {
+            case 0:
+                siteLink = "https://www.carina-core.io";
+                break;
+
+            case 1:
+                siteLink = "https://mobiletesting.farm";
+                break;
+
+            case 2:
+                siteLink = "https://www.qps-infra.io/";
+                break;
+
+            default:
+                siteLink = null;
+        }
+
+        if (siteLink != null) {
+            openLink(siteLink);
+        }
+    }
+
+    private void openLink(String siteLink) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(siteLink));
+        startActivity(browserIntent);
+    }
+
+    private void initImageSlider() {
+        ArrayList<SlideModel> imageList = new ArrayList<>();
+        imageList.add(new SlideModel(R.drawable.img_banner_carina));
+        imageList.add(new SlideModel(R.drawable.img_banner_mcloud));
+        imageList.add(new SlideModel(R.drawable.img_banner_qpsinfra));
+        imageList.add(new SlideModel(R.drawable.img_banner_zafira));
+        imageList.add(new SlideModel(R.drawable.img_banner_zebrunner));
+
+        imageSlider.setImageList(imageList);
+        imageSlider.setItemClickListener(this::handleAdClicked);
     }
 
     private void setInitialScreenState(NavigationView navigationView) {
